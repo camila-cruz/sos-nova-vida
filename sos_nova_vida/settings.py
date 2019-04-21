2504450b38c60e834a09fffa484eae79054c1395
Camila <cruz.camila001@gmail.com>
2019-04-21 14:57:27 -0300

    Adiciona preenchimento automático para o CEP

--- sos_nova_vida/settings.py
+++ sos_nova_vida/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     # App principal
     'sistema',
     'widget_tweaks',
+    'localflavor',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
